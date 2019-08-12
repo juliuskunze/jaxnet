88f6277d4d8137aff13ab4a23c753f2027fd916a
Julius Kunze <juliuskunze@gmail.com>
2019-08-12 21:00:03 +0100

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import pathlib
 
 setuptools.setup(
     name='jaxnet',
-    version='0.1',
+    version='0.1.1',
     description='Neural Nets for JAX',
     url='http://github.com/JuliusKunze/jaxnet',
     long_description=pathlib.Path('README.md').read_text(),
