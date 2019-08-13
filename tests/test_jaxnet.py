252d75bf84407d6f5f22741128425e2d5d004119
Julius Kunze <juliuskunze@gmail.com>
2019-08-13 01:28:43 +0100

    Cleanup tests

--- tests/test_jaxnet.py
+++ tests/test_jaxnet.py
@@ -1,5 +1,6 @@
 import pytest
-from jax import numpy as np, random, jit
+from jax import numpy as np, jit
+from jax.random import PRNGKey
 
 from jaxnet import Dense, Sequential, relu, parameterized, Conv, flatten, MaxPool, zeros, \
     GRUCell, Rnn, softmax
@@ -9,16 +10,16 @@ def test_params():
     net = Dense(2, kernel_init=zeros, bias_init=zeros)
     inputs = np.zeros((1, 3))
 
-    params = net.init_params(random.PRNGKey(0), inputs)
+    params = net.init_params(PRNGKey(0), inputs)
     assert len(params) == 2
-    assert np.array_equal(params.kernel, np.zeros((3, 2)))
-    assert np.array_equal(params.bias, np.zeros(2))
+    assert np.array_equal(np.zeros((3, 2)), params.kernel)
+    assert np.array_equal(np.zeros(2), params.bias)
 
     output = net(params, inputs)
-    assert np.array_equal(output, np.zeros((1, 2)))
+    assert np.array_equal(np.zeros((1, 2)), output)
 
     output_ = jit(net)(params, inputs)
-    assert np.array_equal(output_, output)
+    assert np.array_equal(output, output_)
 
 
 def test_submodule():
@@ -28,35 +29,35 @@ def test_submodule():
 
     inputs = np.zeros((1, 2))
 
-    params = net.init_params(random.PRNGKey(0), inputs)
+    params = net.init_params(PRNGKey(0), inputs)
     assert len(params) == 1
     assert len(params.layer) == 2
-    assert np.array_equal(params.layer.kernel, np.zeros((2, 2)))
-    assert np.array_equal(params.layer.bias, np.zeros(2))
+    assert np.array_equal(np.zeros((2, 2)), params.layer.kernel)
+    assert np.array_equal(np.zeros(2), params.layer.bias)
 
     output = net(params, inputs)
-    assert np.array_equal(output, np.zeros((1, 2)))
+    assert np.array_equal(np.zeros((1, 2)), output)
 
-    output = jit(net)(params, inputs)
-    assert np.array_equal(output, np.zeros((1, 2)))
+    output_ = jit(net)(params, inputs)
+    assert np.array_equal(output, output_)
 
 
 def test_submodule_list():
     layer = Sequential([Dense(2, zeros, zeros), relu])
     inputs = np.zeros((1, 2))
 
-    params = layer.init_params(random.PRNGKey(0), inputs)
+    params = layer.init_params(PRNGKey(0), inputs)
     assert len(params) == 1
     assert len(params.layers) == 2
-    assert np.array_equal(params.layers[0].kernel, np.zeros((2, 2)))
-    assert np.array_equal(params.layers[0].bias, np.zeros(2))
+    assert np.array_equal(np.zeros((2, 2)), params.layers[0].kernel)
+    assert np.array_equal(np.zeros(2), params.layers[0].bias)
     assert params.layers[1] == ()
 
     output = layer(params, inputs)
-    assert np.array_equal(output, np.zeros((1, 2)))
+    assert np.array_equal(np.zeros((1, 2)), output)
 
-    output = jit(layer)(params, inputs)
-    assert np.array_equal(output, np.zeros((1, 2)))
+    output_ = jit(layer)(params, inputs)
+    assert np.array_equal(output, output_)
 
 
 def assert_dense_params_equal(p, p_):
@@ -71,17 +72,17 @@ def test_internal_param_sharing():
         return layer(layer(inputs))
 
     inputs = np.zeros((1, 2))
-    params = shared_net.init_params(random.PRNGKey(0), inputs)
+    params = shared_net.init_params(PRNGKey(0), inputs)
     assert len(params) == 1
     assert len(params.layer) == 2
-    assert np.array_equal(params.layer.kernel, np.zeros((2, 2)))
-    assert np.array_equal(params.layer.bias, np.zeros(2))
+    assert np.array_equal(np.zeros((2, 2)), params.layer.kernel)
+    assert np.array_equal(np.zeros(2), params.layer.bias)
 
     output = shared_net(params, inputs)
-    assert np.array_equal(output, np.zeros((1, 2)))
+    assert np.array_equal(np.zeros((1, 2)), output)
 
-    output = jit(shared_net)(params, inputs)
-    assert np.array_equal(output, np.zeros((1, 2)))
+    output_ = jit(shared_net)(params, inputs)
+    assert np.array_equal(output, output_)
 
 
 def test_internal_param_sharing2():
@@ -91,17 +92,17 @@ def test_internal_param_sharing2():
         return layer(inputs)
 
     inputs = np.zeros((1, 2))
-    params = shared_net.init_params(random.PRNGKey(0), inputs)
+    params = shared_net.init_params(PRNGKey(0), inputs)
 
     assert len(params) == 1
     assert len(params.layer) == 1
     assert len(params.layer.layers) == 2
     assert len(params.layer.layers[0]) == 2
-    assert np.array_equal(params.layer.layers[0].kernel, np.zeros((2, 2)))
-    assert np.array_equal(params.layer.layers[0].bias, np.zeros(2))
+    assert np.array_equal(np.zeros((2, 2)), params.layer.layers[0].kernel)
+    assert np.array_equal(np.zeros(2), params.layer.layers[0].bias)
 
     output = shared_net(params, inputs)
-    assert np.array_equal(output, np.zeros((1, 2)))
+    assert np.array_equal(np.zeros((1, 2)), output)
 
 
 def test_multiple_init_params_calls():
@@ -109,13 +110,14 @@ def test_multiple_init_params_calls():
 
     layer = Dense(5)
     net1 = Sequential([layer, Dense(2)])
-    p1 = net1.init_params(random.PRNGKey(0), inputs)
+    p1 = net1.init_params(PRNGKey(0), inputs)
 
     net2 = Sequential([layer, Dense(3)])
-    p2 = net2.init_params(random.PRNGKey(1), inputs)
+    p2 = net2.init_params(PRNGKey(1), inputs)
 
     assert p1.layers[0].kernel.shape == p2.layers[0].kernel.shape
     assert not np.array_equal(p1.layers[0].kernel, p2.layers[0].kernel)
+
 
 @pytest.mark.skip(reason="TODO reconsider design")
 def test_external_param_sharing():
@@ -123,18 +125,18 @@ def test_external_param_sharing():
     shared_net = Sequential([layer, layer])
 
     inputs = np.zeros((1, 2))
-    params = shared_net.init_params(random.PRNGKey(0), inputs)
+    params = shared_net.init_params(PRNGKey(0), inputs)
     assert len(params) == 1
     assert len(params.layers) == 2
-    assert np.array_equal(params.layers[0].kernel, np.zeros((2, 2)))
-    assert np.array_equal(params.layers[0].bias, np.zeros(2))
+    assert np.array_equal(np.zeros((2, 2)), params.layers[0].kernel)
+    assert np.array_equal(np.zeros(2), params.layers[0].bias)
     assert params.layers[1] == ()
 
     output = shared_net(params, inputs)
-    assert np.array_equal(output, np.zeros((1, 2)))
+    assert np.array_equal(np.zeros((1, 2)), output)
 
     output = jit(shared_net)(params, inputs)
-    assert np.array_equal(output, np.zeros((1, 2)))
+    assert np.array_equal(np.zeros((1, 2)), output)
 
 
 def test_init_params_submodule_reuse():
@@ -144,9 +146,9 @@ def test_init_params_submodule_reuse():
     net1 = Sequential([layer, Dense(2)])
     net2 = Sequential([layer, Dense(3)])
 
-    layer_params = layer.init_params(random.PRNGKey(0), inputs)
-    net1_params = net1.init_params(random.PRNGKey(1), inputs, reuse={layer: layer_params})
-    net2_params = net2.init_params(random.PRNGKey(2), inputs, reuse={layer: layer_params})
+    layer_params = layer.init_params(PRNGKey(0), inputs)
+    net1_params = net1.init_params(PRNGKey(1), inputs, reuse={layer: layer_params})
+    net2_params = net2.init_params(PRNGKey(2), inputs, reuse={layer: layer_params})
     assert_dense_params_equal(layer_params, net1_params.layers[0])
     assert_dense_params_equal(layer_params, net2_params.layers[0])
 
@@ -160,63 +162,63 @@ def test_init_params_submodule_reuse():
 def test_init_params_submodule_reuse_top_level():
     net = Dense(2)
     inputs = np.zeros((1, 3))
-    params = net.init_params(random.PRNGKey(0), inputs)
+    params = net.init_params(PRNGKey(0), inputs)
     output = net(params, inputs)
 
-    params_ = net.init_params(random.PRNGKey(0), inputs, reuse={net: params})
+    params_ = net.init_params(PRNGKey(0), inputs, reuse={net: params})
     assert_dense_params_equal(params, params_)
 
     output_ = net(params_, inputs)
-    assert np.array_equal(output_, output)
+    assert np.array_equal(output, output_)
 
 
 def test_join_params():
     layer = Dense(2)
     net = Sequential([layer, relu])
     inputs = np.zeros((1, 3))
-    layer_params = layer.init_params(random.PRNGKey(0), inputs)
-    # TODO allow: params = (layer_params, ())
+    layer_params = layer.init_params(PRNGKey(0), inputs)
+
     params = net.Parameters((layer_params, ()))
     output = net(params, inputs)
 
     params_ = net.join_params({layer: layer_params})
     assert len(params_) == 1
-    assert_dense_params_equal(params_.layers[0], layer_params)
+    assert_dense_params_equal(layer_params, params_.layers[0])
     assert params_.layers[1] == ()
 
     output_ = net(params_, inputs)
-    assert np.array_equal(output_, output)
+    assert np.array_equal(output, output_)
 
     output_ = net.apply_joined({layer: layer_params}, inputs)
-    assert np.array_equal(output_, output)
+    assert np.array_equal(output, output_)
 
     output_ = net.apply_joined({layer: layer_params}, inputs, jit=True)
-    assert np.array_equal(output_, output)
+    assert np.array_equal(output, output_)
 
 
 @pytest.mark.skip(reason="Unclear if needed")
 def test_join_params_top_level():
     net = Dense(2)
     inputs = np.zeros((1, 3))
-    params = net.init_params(random.PRNGKey(0), inputs)
+    params = net.init_params(PRNGKey(0), inputs)
     output = net(params, inputs)
 
     params_ = net.join_params({net: params})
     assert_dense_params_equal(params, params_)
     output_ = net(params_, inputs)
-    assert np.array_equal(output_, output)
+    assert np.array_equal(output, output_)
 
     output_ = net.apply_joined_params({net: params}, inputs)
-    assert np.array_equal(output_, output)
+    assert np.array_equal(output, output_)
 
     output_ = net.apply_joined_params({net: params}, inputs, jit=True)
-    assert np.array_equal(output_, output)
+    assert np.array_equal(output, output_)
 
 
 def test_example():
     net = Sequential([Dense(2), relu, Dense(4)])
     batch = np.zeros((3, 2))
-    params = net.init_params(random.PRNGKey(0), batch)
+    params = net.init_params(PRNGKey(0), batch)
     print(params.layers[2].bias)
 
     output = net(params, batch)
@@ -227,24 +229,24 @@ def test_conv_flatten():
     conv = Conv(2, filter_shape=(3, 3), padding='SAME', kernel_init=zeros, bias_init=zeros)
     inputs = np.zeros((1, 5, 5, 2))
 
-    params = conv.init_params(random.PRNGKey(0), inputs)
+    params = conv.init_params(PRNGKey(0), inputs)
     output = conv(params, inputs)
-    assert np.array_equal(output, np.zeros((1, 5, 5, 2)))
+    assert np.array_equal(np.zeros((1, 5, 5, 2)), output)
 
     flattened = Sequential([conv, flatten])
     output = flattened({'layers': [params, ()]}, inputs)
-    assert np.array_equal(output, np.zeros((1, 50)))
+    assert np.array_equal(np.zeros((1, 50)), output)
 
 
 def test_conv_max_pool():
     conv = Conv(2, filter_shape=(3, 3), padding='SAME', kernel_init=zeros, bias_init=zeros)
     inputs = np.zeros((1, 5, 5, 2))
 
-    params = conv.init_params(random.PRNGKey(0), inputs)
+    params = conv.init_params(PRNGKey(0), inputs)
     pooled = Sequential([conv, MaxPool(window_shape=(1, 1), strides=(2, 2))])
     inputs = np.zeros((1, 5, 5, 2))
     output = pooled({'layers': [params, ()]}, inputs)
-    assert np.array_equal(output, np.zeros((1, 3, 3, 2)))
+    assert np.array_equal(np.zeros((1, 3, 3, 2)), output)
 
 
 def test_gru_cell():
@@ -252,29 +254,29 @@ def test_gru_cell():
 
     x = np.zeros((2, 3))
     carry = init_carry(batch_size=2)
-    params = gru_cell.init_params(random.PRNGKey(0), carry, x)
+    params = gru_cell.init_params(PRNGKey(0), carry, x)
     output = gru_cell(params, carry, x)
 
     assert isinstance(output, tuple)
     assert len(output) == 2
 
-    assert np.array_equal(output[0], np.zeros((2, 10)))
-    assert np.array_equal(output[1], np.zeros((2, 10)))
+    assert np.array_equal(np.zeros((2, 10)), output[0])
+    assert np.array_equal(np.zeros((2, 10)), output[1])
 
 
 def test_rnn():
     xs = np.zeros((2, 5, 4))
     rnn = Rnn(*GRUCell(3, zeros))
-    params = rnn.init_params(random.PRNGKey(0), xs)
+    params = rnn.init_params(PRNGKey(0), xs)
 
     assert len(params) == 1
     assert len(params.cell) == 3
-    assert np.array_equal(params.cell.update_params, np.zeros((7, 3)))
-    assert np.array_equal(params.cell.reset_params, np.zeros((7, 3)))
-    assert np.array_equal(params.cell.compute_params, np.zeros((7, 3)))
+    assert np.array_equal(np.zeros((7, 3)), params.cell.update_params)
+    assert np.array_equal(np.zeros((7, 3)), params.cell.reset_params)
+    assert np.array_equal(np.zeros((7, 3)), params.cell.compute_params)
 
     output = rnn(params, xs)
-    assert np.array_equal(output, np.zeros((2, 5, 3)))
+    assert np.array_equal(np.zeros((2, 5, 3)), output)
 
 
 def test_rnn_net():
@@ -294,15 +296,15 @@ def test_rnn_net():
         softmax,
         lambda x: np.reshape(x, (-1, length, class_count))])
 
-    params = net.init_params(random.PRNGKey(0), xs)
+    params = net.init_params(PRNGKey(0), xs)
 
     assert len(params) == 1
     assert len(params.layers[0]) == 1
     cell = params.layers[0].cell
     assert len(cell) == 3
-    assert np.array_equal(cell.update_params, np.zeros((7, 3)))
-    assert np.array_equal(cell.reset_params, np.zeros((7, 3)))
-    assert np.array_equal(cell.compute_params, np.zeros((7, 3)))
+    assert np.array_equal(np.zeros((7, 3)), cell.update_params)
+    assert np.array_equal(np.zeros((7, 3)), cell.reset_params)
+    assert np.array_equal(np.zeros((7, 3)), cell.compute_params)
 
     output = net(params, xs)
-    assert np.array_equal(output, np.ones((1, 5, 4)) * .25)
+    assert np.array_equal(.25 * np.ones((1, 5, 4)), output)
